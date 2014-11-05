b6df018b1c0a0d7a9c394fbc0b47ed0de7c22b0f
Justin Pinili <pinili.justin@gmail.com>
2014-11-05 10:30:20 -0800

    (fix) removing extra information.

--- mobileApp/www/js/app.js
+++ mobileApp/www/js/app.js
@@ -61,61 +61,6 @@ angular.module('endevr', ['ionic', 'endevr.controllers', 'ionic.contrib.ui.tinde
   $urlRouterProvider.otherwise('/app/browse');
 })
 
-// .directive('noScroll', function($document) {
-//
-//   return {
-//     restrict: 'A',
-//     link: function($scope, $element, $attr) {
-//
-//       $document.on('touchmove', function(e) {
-//         e.preventDefault();
-//       });
-//     }
-//   }
-// })
-//
-// .controller('CardsCtrl', function($scope, $ionicSwipeCardDelegate) {
-//   var cardTypes = [{
-//     title: 'Swipe down to clear the card',
-//     image: 'img/pic.png'
-//   }, {
-//     title: 'Where is this?',
-//     image: 'img/pic.png'
-//   }, {
-//     title: 'What kind of grass is this?',
-//     image: 'img/pic2.png'
-//   }, {
-//     title: 'What beach is this?',
-//     image: 'img/pic3.png'
-//   }, {
-//     title: 'What kind of clouds are these?',
-//     image: 'img/pic4.png'
-//   }];
-//
-//   $scope.cards = Array.prototype.slice.call(cardTypes, 0, 0);
-//
-//   $scope.cardSwiped = function(index) {
-//     $scope.addCard();
-//   };
-//
-//   $scope.cardDestroyed = function(index) {
-//     $scope.cards.splice(index, 1);
-//   };
-//
-//   $scope.addCard = function() {
-//     var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
-//     newCard.id = Math.random();
-//     $scope.cards.push(angular.extend({}, newCard));
-//   }
-// })
-//
-// .controller('CardCtrl', function($scope, $ionicSwipeCardDelegate) {
-//   $scope.goAway = function() {
-//     var card = $ionicSwipeCardDelegate.getSwipebleCard($scope);
-//     card.swipe();
-//   };
-// });
-
 .directive('noScroll', function($document) {
 
   return {
@@ -130,7 +75,6 @@ angular.module('endevr', ['ionic', 'endevr.controllers', 'ionic.contrib.ui.tinde
 })
 
 .controller('CardsCtrl', function($scope, TDCardDelegate) {
-  console.log('CARDS CTRL');
   var cardTypes = [
     { image: 'img/max.jpg' },
     { image: 'img/ben.png' },
