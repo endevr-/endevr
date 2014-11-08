7400dc9d4820daba7bb446b5f5ec23e923b59bae
jgladch <jeffgladchun@gmail.com>
2014-11-08 12:24:24 -0800

    (refactor) Move AuthCtrl

---
+++ app/www/js/controllers/AuthCtrl.js
@@ -0,0 +1,9 @@
+angular.module('AuthCtrl', ['ionic.contrib.ui.tinderCards', 'LocalStorageModule', 'ionic'])
+
+.controller('AuthCtrl', function($scope, $state, localStorageService){
+  if (localStorageService.get('github-token')) {
+    $scope.Authenticated = true;
+  } else {
+    $scope.needsAuthentication = true;
+  }
+})
