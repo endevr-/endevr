19d806bfeb07744f7466768ab30e3b378b71962c
jgladch <jeffgladchun@gmail.com>
2014-11-08 14:54:45 -0800

    (refactor) Move Services

--- app/www/js/controllers.js
+++
@@ -1,6 +0,0 @@
-angular.module('endevr.controllers', ['ionic.contrib.ui.tinderCards', 'LocalStorageModule', 'ionic'])
-
-.controller('LoginController', function($scope, LinkedInService, GitHubService){
-  $scope.linkedinlogin = LinkedInService.login;
-  $scope.githublogin = GitHubService.login;
-})
