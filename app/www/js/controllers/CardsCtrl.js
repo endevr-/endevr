128a18ea74218fd78a67f43ce564c6b02e9ff6a6
Justin Pinili <pinili.justin@gmail.com>
2014-11-10 22:58:02 -0800

    (feat) fixed ios bug and added local card rendering.

--- app/www/js/controllers/CardsCtrl.js
+++ app/www/js/controllers/CardsCtrl.js
@@ -1,30 +1,20 @@
 angular.module('CardsCtrl', ['ionic.contrib.ui.tinderCards', 'LocalStorageModule', 'ionic'])
 
-.controller('CardsCtrl', function($scope, $http, TDCardDelegate) {
+.controller('CardsCtrl', function($scope, $http, TDCardDelegate, queueService) {
+
   var cardTypes = [];
-
-  //Right now, dev_id is hardcoded. Refactor at first opportunity
-  $http.get('http://localhost:9000/api/developers/XX/cards')
-    .success(function(data){
-      console.log(data);
-      // cardTypes = data;
-      for (var i = 0; i < data.length; i++) {
-        $scope.cards.push(data[i]);
-      }
-    })
-    .error(function(){
-      console.log('Error getting matches');
-    });
-
   $scope.cards = Array.prototype.slice.call(cardTypes, 0);
 
+  var employersQueue = queueService;
+
+  employersQueue.storeTotalCards('http://localhost:9000/api/developers/XX/cards', function(card) {
+    $scope.cards = card;
+  });
+
   $scope.cardDestroyed = function(index) {
-    $scope.cards.splice(index, 1);
+    // $scope.cards.splice(index, 1);
+    console.log('destroyed');
+    employersQueue.removeCard("justin", false);
   };
 
-  $scope.addCard = function() {
-    var newCard = $scope.cards[Math.floor(Math.random() * $scope.cards.length)];
-    newCard.id = Math.random();
-    $scope.cards.push(angular.extend({}, newCard));
-  }
 });
