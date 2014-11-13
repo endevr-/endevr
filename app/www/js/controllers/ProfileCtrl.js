8a41a2074aad44f0ffcadfbd0ac9a0274ebf695d
Justin Pinili <pinili.justin@gmail.com>
2014-11-13 13:43:10 -0800

    connecting the profile controller to the api endpoint of the server.

--- app/www/js/controllers/ProfileCtrl.js
+++ app/www/js/controllers/ProfileCtrl.js
@@ -1,5 +1,19 @@
 angular.module('endevr.controllers')
 
-.controller('ProfileCtrl', function($scope, $ionicModal, $http, $location) {
+.controller('ProfileCtrl', function($scope, $ionicModal, $http, $location, localStorageService) {
 
+  $scope.profile;
+
+  var jwt_token = localStorageService.get('jwt_token');
+  var userType = localStorageService.get('usertype');
+  var profileUrl = 'http://localhost:9000/api/developers/profile?jwt_token=' + jwt_token + '&usertype=' + userType;
+
+  $http.get(profileUrl)
+    .success(function(data) {
+      $scope.profile = data;
+    })
+    .error(function() {
+      console.log('Error getting profile');
+    });
+    
 });
