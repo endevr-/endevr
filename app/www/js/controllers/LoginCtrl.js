33286b9c5e331d944340fee556e907fa37f8db37
jgladch <jeffgladchun@gmail.com>
2014-11-12 16:38:58 -0800

    (Feat) Validate emp login and send to server

--- app/www/js/controllers/LoginCtrl.js
+++ app/www/js/controllers/LoginCtrl.js
@@ -4,12 +4,26 @@ angular.module('endevr.controllers')
   $scope.linkedinlogin = LinkedInService.login;
   $scope.githublogin = GitHubService.login;
   $scope.employer = {};
+  $scope.newEmployer = {};
 
   $scope.employerLogin = function(employer) {
     console.log(employer)
-    $http.post('http://localhost:9000/empLogin', {email: employer.email, password: employer.password, company: employer.company})
-    .success(function(data, status, headers, config){})
-    .error(function(data, status, headers, config){});
+    $http.post('http://localhost:9000/employers/login', {email: employer.email, password: employer.password})
+      .success(function(data, status, headers, config){
+        localStorageService.set('employer-token', data);
+      })
+      .error(function(data, status, headers, config){
+      });
+  };
+
+  $scope.employerSignup = function(employer) {
+    console.log(employer)
+    $http.post('http://localhost:9000/employers/new', {email: employer.email, password: employer.password})
+      .success(function(data, status, headers, config){
+        localStorageService.set('employer-token', data);
+      })
+      .error(function(data, status, headers, config){
+      });
   };
 
   $scope.logout = function () {
