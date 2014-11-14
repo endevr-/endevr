272fafe16adf68938a5f51f67bb1e7c2f5936250
Justin Pinili <pinili.justin@gmail.com>
2014-11-14 12:29:49 -0800

    (feat) adding new items to possible list now works

--- app/www/js/controllers/ProfileCtrl.js
+++ app/www/js/controllers/ProfileCtrl.js
@@ -42,30 +42,22 @@ angular.module('endevr.controllers')
       });
   };
 
-  $ionicModal.fromTemplateUrl('templates/editListItem.html', {
+  /*
+   *    There are 3 different modals created in this section:
+   *      -  Editing List
+   *      -  Edit Existing List Item
+   *      -  Adding New List Item
+   */
 
-    scope: $scope
-
-    })
-    .then(function(modal) {
-
-      $scope.itemModal = modal;
-
-    });
-
-  // Create the modal
+  // Editing List - Modal creation
   $ionicModal.fromTemplateUrl('templates/editList.html', {
-
     scope: $scope
-
     })
     .then(function(modal) {
-
       $scope.modal = modal;
-
     });
 
-  // Open the modal
+  // Editing List - Show Modal
   $scope.showModal = function(category, items) {
 
     // Keeps track of what specific items we are editing
@@ -75,37 +67,27 @@ angular.module('endevr.controllers')
 
   };
 
-  // Close modal and save changes to the database
+  // Editing List - Close modal and save changes to the database
   $scope.saveChanges = function() {
 
     // Turn the items back into an object
     var updatedData = {};
-
     for (var index = 0; index < $scope.items.length; index++) {
       updatedData[""+index] = $scope.items[index];
     }
-
     // Object we are passing back to the database
     var updatedInformation = {
-
         category: $scope.category,
         data: updatedData
-
     };
-
     // POST request to the database
     $http.post(profileUrl, updatedInformation)
       .success(function() {
-
         $scope.getProfile();
-
       })
       .error(function() {
-
         console.log('Error saving profile');
-
       });
-
     // Close modal and set the editing features to false
     $scope.modal.hide();
     $scope.showDelete = false;
@@ -113,7 +95,7 @@ angular.module('endevr.controllers')
 
   };
 
-  // Closes the modal without updating changes
+  // Editing List - Closes the modal without updating changes
   $scope.cancelChanges = function() {
 
     $scope.modal.hide();
@@ -122,7 +104,7 @@ angular.module('endevr.controllers')
 
   };
 
-  // Toggles delete buttons
+  // Editing List - Toggles delete buttons
   $scope.toggleDelete = function() {
 
     $scope.showDelete = !$scope.showDelete;
@@ -130,7 +112,7 @@ angular.module('endevr.controllers')
 
   }
 
-  // Toggles the reorder buttons
+  // Editing List - Toggles the reorder buttons
   $scope.toggleReOrder = function() {
 
     $scope.showReOrder = !$scope.showReOrder;
@@ -138,22 +120,15 @@ angular.module('endevr.controllers')
 
   }
 
-  // Deletes item from the items array
+  // Editing List - Deletes item from the items array
   $scope.onItemDelete = function(item) {
 
     $scope.items.splice( $scope.items.indexOf(item), 1);
 
   };
 
-  // Gives the ability to edit the item value
-  $scope.edit = function(item) {
-    $scope.editItem = item;
-    $scope.oldItemValue = item;
-    $scope.itemModal.show();
 
-  };
-
-  // Changes the order of the items in the array
+  // Editing List - Changes the order of the items in the array
   $scope.moveItem = function(item, fromIndex, toIndex) {
 
     $scope.items.splice(fromIndex, 1);
@@ -161,20 +136,66 @@ angular.module('endevr.controllers')
 
   };
 
+  // Editing List - Gives the ability to edit the item value
+  $scope.edit = function(item) {
+
+    $scope.editItem.value = item;
+    $scope.oldItemValue = item;
+    $scope.itemModal.show();
+
+  };
+
+  // Editing List Item - Create modal
+  $ionicModal.fromTemplateUrl('templates/editListItem.html', {
+    scope: $scope
+    })
+    .then(function(modal) {
+      $scope.itemModal = modal;
+    });
+
+  // Editing List Item - Closes the modal without updating changes
   $scope.closeItemModal = function() {
     $ionicListDelegate.closeOptionButtons();
     $scope.itemModal.hide();
-  }
+  };
 
+  // Editing List Item - Closes the modal with updating changes
   $scope.saveItemModal = function(editItem) {
-    $scope.items[$scope.items.indexOf($scope.oldItemValue)] = editItem;
+    $scope.items[$scope.items.indexOf($scope.oldItemValue)] = editItem.value;
     $ionicListDelegate.closeOptionButtons();
     $scope.itemModal.hide();
-  }
+  };
+
+  // Add New List Item - Create modal
+  $ionicModal.fromTemplateUrl('templates/addListItem.html', {
+    scope: $scope
+    })
+    .then(function(modal) {
+      $scope.newItemModal = modal;
+    });
+
+  // Add New List Item - Open the modal
+  $scope.openNewListItemModal = function() {
+    $scope.newItem.value = '';
+    $scope.newItemModal.show();
+  };
+
+  // Add New List Item - Closes the modal without updating changes
+  $scope.closeNewItemModal = function() {
+    $scope.newItemModal.hide();
+  };
+
+  // Add New List Item = Closes the modal with updating changes
+  $scope.saveNewItemModal = function(newItem) {
+      $scope.items.push(newItem.value);
+      $scope.newItemModal.hide();
+  };
 
   // Initialize default values
   $scope.showDelete = false;
   $scope.showReOrder = false;
+  $scope.editItem = {};
+  $scope.newItem = {};
   $scope.getProfile();
 
 });
