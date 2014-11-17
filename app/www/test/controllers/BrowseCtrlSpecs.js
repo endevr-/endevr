50171c863157109e90383bb48f37d6056b3cbaf6
Adam Back <adb33@email.arizona.edu>
2014-11-17 11:02:01 -0800

    (refactor) Remove failing Browse test bc it's no longer relevant.

--- app/www/test/controllers/BrowseCtrlSpecs.js
+++ app/www/test/controllers/BrowseCtrlSpecs.js
@@ -1,30 +1,15 @@
 describe("BrowseCtrl", function () {
 
-    var $scope, ctrl, $timeout; //, $location;
+    var $scope, ctrl, $timeout, store, ls; //, $location;
 
     beforeEach(function () {
         module('endevr');
+        store= {};
 
-        // INJECT! This part is critical
-        // $rootScope - injected to create a new $scope instance.
-        // $controller - injected to create an instance of our controller.
-        // $q - injected so we can create promises for our mocks.
-        // _$timeout_ - injected to we can flush unresolved promises.
         inject(function ($rootScope, $controller, $q, _$timeout_) {
-
-            // create a scope object for us to use.
             $scope = $rootScope.$new();
-            // $location = $injector.get('$location');
-            // assign $timeout to a scoped variable so we can use
-            // $timeout.flush() later. Notice the _underscore_ trick
-            // so we can keep our names clean in the tests.
             $timeout = _$timeout_;
 
-            // now run that scope through the controller function,
-            // injecting any services or other injectables we need.
-            // **NOTE**: this is the only time the controller function
-            // will be run, so anything that occurs inside of that
-            // will already be done before the first spec.
             ctrl = $controller('BrowseCtrl', {
                 $scope: $scope
             });
@@ -32,10 +17,6 @@ describe("BrowseCtrl", function () {
 
     });
 
-
-    // Test 1: The simplest of the simple.
-    // here we're going to make sure the $scope variable 
-    // exists evaluated.
     it("should have a $scope variable", function() {
         expect($scope).toBeDefined();
     });
@@ -44,8 +25,6 @@ describe("BrowseCtrl", function () {
         expect($scope.interest).toBeDefined();
     });
 
-    it('interest currently should be hard-coded to \'Employers\'', function() {
-      expect($scope.interest).toEqual('Employers');
-    });
-
+    // figure out how to mock local storage to check logic
+    // Maybe: http://ngokevin.com/blog/angular-unit-testing/
 });
