126d902a9445be7aaa41c8976ddc1c56998ef507
Adam Back <adb33@email.arizona.edu>
2014-11-18 21:19:46 -0800

    (feat) Pull matches from database and display for developers.

--- app/www/js/controllers/MatchesCtrl.js
+++ app/www/js/controllers/MatchesCtrl.js
@@ -33,10 +33,11 @@ angular.module('endevr.controllers')
 
   $scope.getMatches = function() {
     var url;
-    var jwt_token = localStorageService.get('jwt-token');
+    var fields;
+    var jwt_token = localStorageService.get('jwt_token');
     // check local storage to get user type
     if ($scope.type === 'dev') {
-      url = 'http://localhost:9000/api/developers/matches?jwt_token=' + jwt_token;
+      url = 'http://localhost:9000/api/developers/matches?jwt_token=' + jwt_token + '&usertype=dev';
       // get matches from /developers/matches
       $http.get(url)
         .success(function(data) {
@@ -47,10 +48,10 @@ angular.module('endevr.controllers')
           }
         })
         .error(function() {
-          console.log('Error getting matches');
+          alert("error");
         });
     } else if ($scope.type === 'emp') {
-      url = 'http://localhost:9000/api/employers/matches?jwt_token=' + jwt_token;
+      url = 'http://localhost:9000/api/employers/matches?jwt_token=' + jwt_token + '&usertype=emp';
       $http.get(url)
         .success(function(data) {
           for (var matchedDev = 0; matchedDev < data.length; matchedDev++) {
@@ -64,5 +65,5 @@ angular.module('endevr.controllers')
   };
 
   // call matches on controller load
-  // $scope.getMatches();
+  $scope.getMatches();
 });
