2673804cf8a083fafb3f2347b86421c962138c63
Justin Pinili <pinili.justin@gmail.com>
2014-11-19 10:53:16 -0800

    (fix) previous change effected dev view. now only should influence the emp view.

--- app/www/js/controllers/AppCtrl.js
+++ app/www/js/controllers/AppCtrl.js
@@ -2,12 +2,20 @@ angular.module('endevr.controllers')
 
 .controller('AppCtrl', function($scope, $location, localStorageService, $ionicNavBarDelegate) {
 
+  $scope.current = 'browse';
+
   $scope.logout = function() {
     localStorageService.clearAll();
     $location.path('/app/auth');
   };
 
   $scope.navigate = function(route) {
+    if ($scope.current !== route) {
+      $scope.current = route;
+    } else {
+      $scope.current = route + 'again';
+    }
+
     if (route === 'profile') {
       $location.path('/app/'+localStorageService.get('usertype')+'profile');
     } else {
