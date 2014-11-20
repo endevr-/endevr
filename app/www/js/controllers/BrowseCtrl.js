0f9e92b7cf24079586de42488a9250ae34616450
Justin Pinili <pinili.justin@gmail.com>
2014-11-20 13:43:10 -0800

    (fix) removing alert

--- app/www/js/controllers/BrowseCtrl.js
+++ app/www/js/controllers/BrowseCtrl.js
@@ -61,7 +61,6 @@ angular.module('endevr.controllers')
         } else {
           $scope.noJobs = false;
         }
-        alert($scope.noJobs);
         $scope.positions = data;
       })
       .error(function() {
