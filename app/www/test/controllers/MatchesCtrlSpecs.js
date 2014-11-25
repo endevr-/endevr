ba142536f59c664190135b3149196b404aa9c7c6
Adam Back <adb33@email.arizona.edu>
2014-11-25 14:22:00 -0800

    (test) Rebuild MatchCtrlSpec framework. At 43%. Test through showModal for developers.

--- app/www/test/controllers/MatchesCtrlSpecs.js
+++ app/www/test/controllers/MatchesCtrlSpecs.js
@@ -1,39 +1,218 @@
 describe('MatchesCtrl', function() {
-  var $scope, ctrl, $location, $httpBackend, localStorageService, $ionicModal;
-  
-  beforeEach(function() {
-    module('endevr');
+  describe('for developers', function() {
+    describe('with success', function() {
+      var $scope, ctrl, $location, $httpBackend, localStorageService, $ionicModal;
+      
+      beforeEach(function() {
+        module('endevr');
 
-    inject(function($rootScope, $controller, _localStorageService_, _$httpBackend_, _$location_) {
-      $scope = $rootScope.$new();
-      ctrl = $controller('MatchesCtrl', {
-                $scope: $scope
+        inject(function($rootScope, $controller, _localStorageService_, _$httpBackend_, _$location_) {
+          $scope = $rootScope.$new();
+          ctrl = $controller('MatchesCtrl', {
+            $scope: $scope
+          });
+
+          var Modal = function() {void(0);}
+          Modal.prototype.show = function() {void(0);};
+          Modal.prototype.hide = function() {void(0);};
+          $scope.devProfileModal = new Modal();
+          $scope.empProfileModal = new Modal();
+
+          $httpBackend = _$httpBackend_;
+          $httpBackend.whenGET('templates/profile.html').respond('');
+          $httpBackend.whenGET('templates/auth.html').respond('');
+          $httpBackend.whenGET('templates/matches.html').respond('');
+          $httpBackend.whenGET('templates/card.html').respond('');
+          $httpBackend.whenGET('templates/cards.html').respond('');
+          $httpBackend.whenGET('templates/empprofile.html').respond('');
+          $httpBackend.whenGET('templates/menu.html').respond('');
+          $httpBackend.whenGET('templates/tutorialModal.html').respond('');
+          $httpBackend.whenGET('templates/browse.html').respond('');
+          $httpBackend.whenGET('templates/devProfileModal.html').respond('');
+          $httpBackend.whenGET('templates/empProfileModal.html').respond('');
+          $httpBackend.flush();
+        });
+      });
+      
+      afterEach(function() {
+           $httpBackend.verifyNoOutstandingExpectation();
+           $httpBackend.verifyNoOutstandingRequest();
       });
 
-      $httpBackend = _$httpBackend_;
-      $httpBackend.whenGET('templates/profile.html').respond('');
-      $httpBackend.whenGET('templates/auth.html').respond('');
-      $httpBackend.whenGET('templates/matches.html').respond('');
-      $httpBackend.whenGET('templates/card.html').respond('');
-      $httpBackend.whenGET('templates/cards.html').respond('');
-      $httpBackend.whenGET('templates/empprofile.html').respond('');
-      $httpBackend.whenGET('templates/menu.html').respond('');
-      $httpBackend.whenGET('templates/tutorialModal.html').respond('');
-      $httpBackend.whenGET('templates/browse.html').respond('');
-      $httpBackend.whenGET('templates/devProfileModal.html').respond('');
-      $httpBackend.whenGET('templates/empProfileModal.html').respond('');
-      $httpBackend.flush();
+      it('should have a defined scope', function() {
+        expect($scope).toBeDefined();
+      });
+
+      describe('decide', function() {
+            it('should be defined as a function', function() {
+              expect(angular.isFunction($scope.decide)).toBe(true);
+            });
+
+            it('should set the posid on scope', function() {
+              $scope.posid = 1;
+              $scope.decide(2);
+              expect($scope.posid).toBe(2);
+            });
+
+            it('should set the posid on rootScope', function() {
+              inject(function($rootScope) {
+                $rootScope.posid = 1;
+                $scope.decide(2);
+                expect($rootScope.posid).toBe(2);
+              });
+            });
+
+            it('should have called getMatches', function() {
+              spyOn($scope, 'getMatches');
+              $scope.decide(1);
+              expect($scope.getMatches).toHaveBeenCalled();
+            });
+      });
+
+      describe('showModal', function() {
+        it('should be defined as a function', function() {
+          expect(angular.isFunction($scope.showModal)).toBe(true);
+        });
+
+        it('should set the profile variable', function() {
+          $scope.showModal("Adam");
+          expect($scope.profile).toBe("Adam");
+        });
+
+        it('should show the employer\'s profile modal', function() {
+          $scope.type = 'dev';
+          spyOn($scope.empProfileModal, 'show');
+          $scope.showModal("Adam");
+          expect($scope.empProfileModal.show).toHaveBeenCalled();
+        });
+
+        it('should show the developer\'s profile modal', function() {
+          $scope.type = 'emp';
+          spyOn($scope.devProfileModal, 'show');
+          $scope.showModal("Adam");
+          expect($scope.devProfileModal.show).toHaveBeenCalled();
+        });
+      });
+
+    });
+
+    describe('with failure', function() {
+      var $scope, ctrl, $location, $httpBackend, localStorageService, $ionicModal;
+      
+      beforeEach(function() {
+        module('endevr');
+
+        inject(function($rootScope, $controller, _localStorageService_, _$httpBackend_, _$location_) {
+          $scope = $rootScope.$new();
+          ctrl = $controller('MatchesCtrl', {
+                    $scope: $scope
+          });
+
+          $httpBackend = _$httpBackend_;
+          $httpBackend.whenGET('templates/profile.html').respond('');
+          $httpBackend.whenGET('templates/auth.html').respond('');
+          $httpBackend.whenGET('templates/matches.html').respond('');
+          $httpBackend.whenGET('templates/card.html').respond('');
+          $httpBackend.whenGET('templates/cards.html').respond('');
+          $httpBackend.whenGET('templates/empprofile.html').respond('');
+          $httpBackend.whenGET('templates/menu.html').respond('');
+          $httpBackend.whenGET('templates/tutorialModal.html').respond('');
+          $httpBackend.whenGET('templates/browse.html').respond('');
+          $httpBackend.whenGET('templates/devProfileModal.html').respond('');
+          $httpBackend.whenGET('templates/empProfileModal.html').respond('');
+          $httpBackend.flush();
+        });
+      });
+      
+      afterEach(function() {
+           $httpBackend.verifyNoOutstandingExpectation();
+           $httpBackend.verifyNoOutstandingRequest();
+      });
+
+      it('should have a defined scope', function() {
+        expect($scope).toBeDefined();
+      });
+    }); 
+  });
+
+  describe('for employers', function() {
+    describe('with success', function() {
+      var $scope, ctrl, $location, $httpBackend, localStorageService, $ionicModal;
+      
+      beforeEach(function() {
+        module('endevr');
+
+        inject(function($rootScope, $controller, _localStorageService_, _$httpBackend_, _$location_) {
+          $scope = $rootScope.$new();
+          ctrl = $controller('MatchesCtrl', {
+                    $scope: $scope
+          });
+
+          $httpBackend = _$httpBackend_;
+          $httpBackend.whenGET('templates/profile.html').respond('');
+          $httpBackend.whenGET('templates/auth.html').respond('');
+          $httpBackend.whenGET('templates/matches.html').respond('');
+          $httpBackend.whenGET('templates/card.html').respond('');
+          $httpBackend.whenGET('templates/cards.html').respond('');
+          $httpBackend.whenGET('templates/empprofile.html').respond('');
+          $httpBackend.whenGET('templates/menu.html').respond('');
+          $httpBackend.whenGET('templates/tutorialModal.html').respond('');
+          $httpBackend.whenGET('templates/browse.html').respond('');
+          $httpBackend.whenGET('templates/devProfileModal.html').respond('');
+          $httpBackend.whenGET('templates/empProfileModal.html').respond('');
+          $httpBackend.flush();
+        });
+      });
+      
+      afterEach(function() {
+           $httpBackend.verifyNoOutstandingExpectation();
+           $httpBackend.verifyNoOutstandingRequest();
+      });
+
+      it('should have a defined scope', function() {
+        expect($scope).toBeDefined();
+      });
+    });
+
+    describe('with failure', function() {
+      var $scope, ctrl, $location, $httpBackend, localStorageService, $ionicModal;
+      
+      beforeEach(function() {
+        module('endevr');
+
+        inject(function($rootScope, $controller, _localStorageService_, _$httpBackend_, _$location_) {
+          $scope = $rootScope.$new();
+          ctrl = $controller('MatchesCtrl', {
+                    $scope: $scope
+          });
+
+          $httpBackend = _$httpBackend_;
+          $httpBackend.whenGET('templates/profile.html').respond('');
+          $httpBackend.whenGET('templates/auth.html').respond('');
+          $httpBackend.whenGET('templates/matches.html').respond('');
+          $httpBackend.whenGET('templates/card.html').respond('');
+          $httpBackend.whenGET('templates/cards.html').respond('');
+          $httpBackend.whenGET('templates/empprofile.html').respond('');
+          $httpBackend.whenGET('templates/menu.html').respond('');
+          $httpBackend.whenGET('templates/tutorialModal.html').respond('');
+          $httpBackend.whenGET('templates/browse.html').respond('');
+          $httpBackend.whenGET('templates/devProfileModal.html').respond('');
+          $httpBackend.whenGET('templates/empProfileModal.html').respond('');
+          $httpBackend.flush();
+        });
+      });
+      
+      afterEach(function() {
+           $httpBackend.verifyNoOutstandingExpectation();
+           $httpBackend.verifyNoOutstandingRequest();
+      });
+
+      it('should have a defined scope', function() {
+        expect($scope).toBeDefined();
+      });
     });
   });
 
-  it('should have a defined scope', function() {
-    expect($scope).toBeDefined();
-  });
-
-  afterEach(function() {
-       $httpBackend.verifyNoOutstandingExpectation();
-       $httpBackend.verifyNoOutstandingRequest();
-  });
 });
 // when controller initializes
   // for developers
